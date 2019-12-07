87386c5cee6218228a9de448774d50168ee30730
Stonino82 <antoninlattene@gmail.com>
2019-12-07 18:16:11 +0100

    navigation effects and other stuff

--- src/js/default.js
+++ src/js/default.js
@@ -1,5 +1,6 @@
-jQuery(function ($) {
+jQuery(function ($) {   // All scripts after this line (with this we can use jQuery symbol"$" inside Wordpress!!)
     
+
     $(document).ready(function() {
 
         // one page navigation plugin
@@ -23,8 +24,74 @@ jQuery(function ($) {
 
     });
 
-    $(document).scroll(function() {
-        $('.js-nav').toggleClass('nav--sticked', $(document).scrollTop() >= 20);
+    // var home = $('#section__home').outerHeight();
+    // console.log(home);
+
+    // $(window).scroll(function() {
+    //     $('.js-nav').toggleClass('nav--sticked', $(document).scrollTop() >= 20);
+    // });
+
+
+
+    /*********************************** NAVIGATION EFFECT *****************************************/
+    // Add .nav--sticked after X scroll
+    var flag = false;
+    $(window).scroll(function () { 
+    var scroll = $(this).scrollTop();
+    if (scroll >= $('#section__home').outerHeight() ) { //After section section__home
+        if(!flag){
+        $('nav').addClass('nav--sticked');
+        flag = true;
+        }
+    }
+    else {
+        if(flag){
+        $('nav').removeClass('nav--sticked');
+        flag = false;
+        }
+    }
     });
 
-});
+    // Hide Nav on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 0;
+    var navbarHeight = $('nav').outerHeight();
+
+    $(window).scroll(function(event){
+    didScroll = true;
+    });
+
+    setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+    }, 50);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+        
+        // If scrolled down and past the navbar, add class .nav-up.
+        // This is needed so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('.nav--sticked').removeClass('nav-down').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+            $('.nav--sticked').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+
+        lastScrollTop = st;
+        }
+    /*********************************** NAVIGATION EFFECT *****************************************/
+
+
+
+}); // All scripts before this line
